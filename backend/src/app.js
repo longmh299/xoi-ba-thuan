8e2f56315b1ccfdc36e85782cc02d69913230323
longmh299 <mailonggt2020@gmail.com>
2026-06-30 10:02:22 +0700

    refactor: setup backend structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Xoi Ba Thuan API");
+    res.json({
+        success: true,
+        message: "Xôi Bà Thuận API"
+    });
 });
 
 export default app;
